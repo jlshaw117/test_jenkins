0f7aa74ede25db60e892905d8d0560f092bd199c
Jerrik Shaw <jlshaw117@gmail.com>
2020-08-27 10:51:18 -0700

    Fix test

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ class TestMySum(unittest.TestCase):
     def test_diff_list(self):
         data = [5,6,7]
         result = my_sum(data)
-        self.assertEqual(result, 10)
+        self.assertEqual(result, 18)
 
 if __name__ == '__main__':
     unittest.main()
